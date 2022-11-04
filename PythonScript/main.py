f529af86857abfd6154407a88ad459889ef30280
Zijie Yu <zijieuu@gmail.com>
2022-11-03 21:04:39 -0700

    update

--- PythonScript/main.py
+++ PythonScript/main.py
@@ -5,24 +5,25 @@ import os
 # connect to the AirSim simulator
 client = airsim.MultirotorClient()
 client.confirmConnection()
-client.enableApiControl(True)
-client.armDisarm(True)
+client.enableApiControl(True, "UAV2")
+client.armDisarm(True, "UAV2")
 
-# Async methods returns Future. Call join() to wait for task to complete.
-client.takeoffAsync().join()
-client.moveToPositionAsync(-10, 10, -10, 5).join()
+client.takeoffAsync(vehicle_name="UAV2").join()  # takeoff
 
-# take images
-responses = client.simGetImages([
-    airsim.ImageRequest("0", airsim.ImageType.DepthVis),
-    airsim.ImageRequest("1", airsim.ImageType.DepthPlanar, True)])
-print('Retrieved images: %d', len(responses))
+# square flight
+client.moveToZAsync(-3, 1, vehicle_name="UAV2").join()  # 上升到3m高度
+client.moveToPositionAsync(5, 0, -3, 1, vehicle_name="UAV2").join()  # 飞到（5,0）点坐标
+client.moveToPositionAsync(5, 5, -3, 1, vehicle_name="UAV2").join()  # 飞到（5,5）点坐标
+client.moveToPositionAsync(0, 5, -3, 1, vehicle_name="UAV2").join()  # 飞到（0,5）点坐标
+client.moveToPositionAsync(0, 0, -3, 1, vehicle_name="UAV2").join()  # 回到（0,0）点坐标
 
-# do something with the images
-for response in responses:
-    if response.pixels_as_float:
-        print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
-        airsim.write_pfm(os.path.normpath('/temp/py1.pfm'), airsim.get_pfm_array(response))
-    else:
-        print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
-        airsim.write_file(os.path.normpath('/temp/py1.png'), response.image_data_uint8)
+client.landAsync(vehicle_name="UAV2").join()  # land
+client.armDisarm(False)  # lock
+client.enableApiControl(False)  # release control
+
+
+
+
+
+# for i in range(9):
+#     name = "UAV"+str(i+1)
